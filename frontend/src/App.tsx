789246411e7931ea4273e2458226c74fb25a75b0
webgenapp <87999663+webgenapp@users.noreply.github.com>
2021-08-06 20:49:30 +0200

    Create app using webgen.dev

---
+++ frontend/src/App.tsx
@@ -0,0 +1,90 @@
+import React, { useEffect } from 'react'
+
+import CreateCat from './cats/CreateCat'
+import ListCat from './cats/ListCat'
+import DetailCat from './cats/DetailCat'
+import UpdateCat from './cats/UpdateCat'
+
+import CreateUser from './users/CreateUser'
+import ListUser from './users/ListUser'
+import DetailUser from './users/DetailUser'
+import UpdateUser from './users/UpdateUser'
+
+import LoginForm from './auth/LoginForm'
+import RegisterForm from './auth/RegisterForm'
+
+import { Route, Switch, BrowserRouter as Router, Link } from 'react-router-dom'
+
+import { useQuery } from 'react-query'
+
+import client, { fetchCSRFToken, hasCSRFToken } from './api'
+
+function App() {
+  const { data: user } = useQuery('user', () => client.get('/auth/me'), {
+    retry: false,
+  })
+
+  useEffect(() => {
+    if (!hasCSRFToken()) fetchCSRFToken()
+  }, [])
+
+  return (
+    <Router>
+      <nav>
+        <ul className='flex'>
+          <li>
+            <Link to='/'>Home</Link>
+            <br />
+          </li>
+
+          <li className='ml-10'>
+            <Link to='/login'>Login</Link>
+            <br />
+            <Link to='/register'>Register</Link>
+            <br />
+          </li>
+
+          <li className='ml-10'>
+            <Link to='/cats'>Cats</Link>
+            <br />
+            <Link to='/cats/create'>Create a Cat</Link>
+            <br />
+          </li>
+
+          <li className='ml-10'>
+            <Link to='/users'>Users</Link>
+            <br />
+            <Link to='/users/create'>Create a User</Link>
+            <br />
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <Route path='/cats'>
+          <h1>Cats</h1>
+        </Route>
+
+        <Route path='/users'>
+          <h1>Users</h1>
+        </Route>
+
+        <Switch>
+          {/* Cat routes */}
+          <Route path='/cats/create' component={CreateCat} />
+          <Route path='/cats/update/:id' component={UpdateCat} />
+          <Route path='/cats/detail/:id' component={DetailCat} />
+          <Route path='/cats' component={ListCat} />,{/* User routes */}
+          <Route path='/users/create' component={CreateUser} />
+          <Route path='/users/update/:id' component={UpdateUser} />
+          <Route path='/users/detail/:id' component={DetailUser} />
+          <Route path='/users' component={ListUser} />
+          {/* auth routes */}
+          <Route path='/login' component={LoginForm} />
+          <Route path='/register' component={RegisterForm} />
+        </Switch>
+      </main>
+    </Router>
+  )
+}
+
+export default App
